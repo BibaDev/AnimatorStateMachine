2a09f80d90f68070e42fb17b3c342f0173d4a34e
Mikhail <59769710+BibaDev@users.noreply.github.com>
2021-02-05 15:36:41 +0300

    Add files via upload

---
+++ IAnimatorChangeStateHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Created by BibaDev: (https://biba.dev)
+ * Support me (donation): https://biba.dev/donation
+ * Contact me: https://biba.dev/feedback
+ * YouTube channel: https://bit.ly/39Wbgrj
+ * GitHub: https://github.com/BibaDev/AnimatorStateMachine
+*/
+
+namespace BibaDev.AnimatorStateMachine
+{
+    
+    public interface IAnimatorChangeStateHandler<in TEnum> where TEnum : Enum
+    {
+
+        void OnAnimatorStateChanged(TEnum id);
+
+    }
+
+}
